ba6177d5cede80f31c288c6ced5d879c130d2612
Clément SOUIL <souil.clement@outlook.fr>
2026-04-01 11:46:16 +0200

    Ajout models Comment et Interaction

---
+++ src/models/Interaction.ts
@@ -0,0 +1,19 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+
+export interface IInteraction extends Document {
+    UserId: mongoose.Schema.Types.ObjectId;
+    date: Date;
+}
+
+
+const InteractionSchema = new mongoose.Schema<IInteraction>({
+    UserId: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User',    
+    },
+    date: { 
+        type: Date, 
+        default: Date.now 
+    }
+
+});
