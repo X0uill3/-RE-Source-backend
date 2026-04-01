9e9a4ac2ff144fc250a642ceed8d7a7218058213
Enzo Agostinho <agostinhoenzo11@gmail.com>
2026-04-01 12:01:26 +0200

    Models edit 1

--- src/models/Comment.ts
+++ src/models/Comment.ts
@@ -3,6 +3,10 @@ import mongoose, { Document, Model, Schema } from "mongoose";
 export interface IComment extends Document {
     content: StaticRange;
     date: Date;
+    systemStatus: 'Enabled' | 'Disabled';
+    authorId: mongoose.Schema.Types.ObjectId;
+    ressourceId: mongoose.Schema.Types.ObjectId;
+    commentId?: mongoose.Schema.Types.ObjectId;
 }
 
 const CommentSchema = new mongoose.Schema<IComment>({
@@ -10,12 +14,32 @@ const CommentSchema = new mongoose.Schema<IComment>({
         type: String,
         required: true,
         trim: true,
-        defaut: 'Enabled'
     },
     date: {
         type: Date,
         default: Date.now
+    },
+    systemStatus: {
+        type: String,
+        enum: ['Enabled', 'Disabled'],
+        default: 'Enabled'
+    },
+    authorId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    ressourceId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Ressource',
+        required: true
+    },
+    commentId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment',
+        default: null
     }
+
 });
 
 const CommentModel: Model<IComment> = mongoose.model<IComment>("Comment", CommentSchema);
--- src/models/Interaction.ts
+++ src/models/Interaction.ts
@@ -1,8 +1,11 @@
 import mongoose, { Document, Model, Schema } from "mongoose";
+import { RessourceInteractionType } from "../constants/interactions.js";
 
 export interface IInteraction extends Document {
     UserId: mongoose.Schema.Types.ObjectId;
     date: Date;
+    interactionType: RessourceInteractionType;
+    ressourceId: mongoose.Schema.Types.ObjectId;
 }
 
 
@@ -14,8 +17,17 @@ const InteractionSchema = new mongoose.Schema<IInteraction>({
     date: {
         type: Date,
         default: Date.now
+    },
+    interactionType: {
+        type: String,
+        enum: Object.values(RessourceInteractionType),
+        required: true
+    },
+    ressourceId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Ressource',
+        required: true
     }
-
 });
 
 const InteractionModel: Model<IInteraction> = mongoose.model<IInteraction>("Interaction", InteractionSchema);
