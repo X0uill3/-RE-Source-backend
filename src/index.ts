93b08766cae67beeba3cd6c7bae8cdad4bba6cf3
Enzo Agostinho <agostinhoenzo11@gmail.com>
2026-04-02 10:32:33 +0200

    Sorry

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
-import express from 'express';
+import express from "express";
 // En mode 'verbatimModuleSyntax', on sépare bien les types
-import type { Application, Request, Response } from 'express';
-import cors from 'cors';
-import * as dotenv from 'dotenv';
-import connectDB from './config/db.js';
+import type { Application, Request, Response } from "express";
+import cors from "cors";
+import * as dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
-import userRoutes from './routes/userRoutes.js';
-import authRoutes from './routes/authRoutes.js';
-import categorieRoutes from './routes/categorieRoutes.js';
-import typeRelationRoutes from './routes/typeRelationRoutes.js';
-import ressourceRoutes from './routes/ressourceRoutes.js';
-import commentRoutes from './routes/commentRoutes.js';
-import interactionRoutes from './routes/interactionRoutes.js'
-import { protect, softProtect } from './middleware/authMiddleware.js';
-
+import userRoutes from "./routes/userRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import categorieRoutes from "./routes/categorieRoutes.js";
+import typeRelationRoutes from "./routes/typeRelationRoutes.js";
+import ressourceRoutes from "./routes/ressourceRoutes.js";
+import commentRoutes from "./routes/commentRoutes.js";
+import interactionRoutes from "./routes/interactionRoutes.js";
+import { protect, softProtect } from "./middleware/authMiddleware.js";
 
 dotenv.config();
 connectDB();
@@ -25,20 +24,25 @@ app.use(express.json());
 
 app.use(softProtect);
 
-app.use('/api/users', userRoutes);
-app.use('/api/auth', authRoutes);
-app.use('/api/categories', categorieRoutes);
-app.use('/api/typeRelation', typeRelationRoutes);
-app.use('/api/resources', ressourceRoutes);
-app.use('/api/comments', commentRoutes);
-app.use('/api/interactions', interactionRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/categories", categorieRoutes);
+app.use("/api/typeRelation", typeRelationRoutes);
+app.use("/api/resources", ressourceRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/interactions", interactionRoutes);
 
 // Ajoute bien les types ici pour que res.send() soit reconnu
-app.get('/', (req: Request, res: Response) => {
-    res.send('API Ressource Backend est en cours d\'exécution');
+app.get("/", (req: Request, res: Response) => {
+    res.send("API Ressource Backend est en cours d'exécution");
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`✅ Serveur démarré sur le port ${PORT}`);
-});
+if (process.env.NODE_ENV !== "test") {
+    connectDB();
+    app.listen(PORT, () => {
+        console.log(`✅ Serveur démarré sur le port ${PORT}`);
+    });
+}
+
+export default app; // Export de l'app pour les tests d'intégration
